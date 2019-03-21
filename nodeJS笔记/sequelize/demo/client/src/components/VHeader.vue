2fccc5d0f5e234d787fb24e9ff698d4827b0b58f
zxswin <894398509@qq.com>
2019-03-22 06:23:08 +0800

    'app-demo'

---
+++ nodeJS笔记/sequelize/demo/client/src/components/VHeader.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <div class="py-5 text-center">
+            <img class="d-block mx-auto mb-4" src="../assets/images/sequelize.jpg" />
+            <p class="lead">
+                爱就好比骑马和学法语，如果不趁年轻时学会，以后想学会就难了。
+            </p>
+            <p class="lead">
+                Love is like riding or speaking French,if you don not learn it young, it's hard to get the trick of it
+                later.
+            </p>
+            <!-- 人这一生，总要心碎一两次的。
+
+                We must have our heart broken once or twice before we are done.
+                世事无常，我们要随遇而安。
+
+                It just happens, and we should live with it.
+
+                人生就是不断收集回忆的过程，最终陪伴我们的也只有回忆了。
+
+                The business of life is the acquisition of memories.In the end that's all there is.
+
+                亲爱的，人生总是会遇到各种麻烦，我们得尽力去解决它。
+                My dear, all life is a series of problems which we must try and solve.
+
+                没有翻不了的山，没有沉不了的船。
+                Every mountain is unclimbable until someone climbs it.Every ship is unsinkable until it sinks.
+
+                世道变了，我们也得跟着变。
+
+                The world moves on, and we must move with it.
+
+                当坏事发生时，仍希望未发生过是毫无意义的，当务之急是减少损失。
+
+                When something bad happens,there is no point in wishing it had not happened.The only option is to minimize the damage.
+                每种生活都有它自己的规矩，如果你不愿意遵守，那么这种生活就不适合你。 -->
+        </div>
+
+        <div class="heading text-right mb">
+            <a href="">注册</a>
+            <span> | </span>
+            <a href="">登录</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
